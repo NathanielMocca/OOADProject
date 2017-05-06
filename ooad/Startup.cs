8ab84da556ee5e36c6fa83197694ad8c210a87d4
KadenzWei <galadiya41@gmail.com>
2017-05-06 17:07:54 +0800

    First Commit

---
+++ ooad/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ooad.Startup))]
+namespace ooad
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
